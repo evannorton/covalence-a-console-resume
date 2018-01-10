6fd860e60cdf5c1e64d61eb82a9a73ff45c5a63a
Evan Norton <evanan39@yahoo.com>
2018-01-10 14:37:37 -0600

    displaySkill function

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("Evan Norton".toUpperCase());
 function displayPosition(company, job, desc) {
     console.log(job + " at " + company + " - " + desc);
 }
+function displaySkill(skill isCool) {
+    if (isCool) {
+        console.log("BAM: " + skill);
+    } else {
+        console.log(skill);
+    }
+}
