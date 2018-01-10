7ff595f81e198df255f637f70316832203b104fa
Evan Norton <evanan39@yahoo.com>
2018-01-10 14:50:22 -0600

    skills

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function displayPosition(company, job, desc) {
-    console.log(job + " at " + company + " - " + desc);
+    console.log("* " + job + " at " + company + " - " + desc);
 }
 function displaySkill(skill, isCool) {
     if (isCool) {
-        console.log("BAM: " + skill);
+        console.log("* BAM: " + skill);
     } else {
-        console.log(skill);
+        console.log("* " + skill);
     }
 }
 
@@ -22,3 +22,10 @@ displayPosition("fake company 1", "fake job 1", "ummmmm did stuff")
 displayPosition("fake company 2", "fake job 2", "ummmmm did stuff")
 displayPosition("fake company 3", "fake job 3", "ummmmm did stuff")
 displayPosition("fake company 4", "fake job 4", "ummmmm did stuff")
+console.log("My skills:");
+displaySkill("programming", true);
+displaySkill("sleeping", false);
+displaySkill("eating", false);
+displaySkill("playing guitar", true);
+displaySkill("walking", false);
+displaySkill("more sleeping", false);
