34a385b01a31d0e8dba55610e29686dea01ef42d
Evan Norton <evanan39@yahoo.com>
2018-01-10 14:47:58 -0600

    experiences

--- app.js
+++ app.js
@@ -17,3 +17,8 @@ console.log("* wife");
 console.log("* music");
 console.log("* programming");
 console.log("* video games");
+console.log("My previous experiences:");
+displayPosition("fake company 1", "fake job 1", "ummmmm did stuff")
+displayPosition("fake company 2", "fake job 2", "ummmmm did stuff")
+displayPosition("fake company 3", "fake job 3", "ummmmm did stuff")
+displayPosition("fake company 4", "fake job 4", "ummmmm did stuff")
